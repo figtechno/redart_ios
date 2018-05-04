4881c1a3ae89bc1a16ba73de99c47ce9cf16333e
figtechno <35449922+figtechno@users.noreply.github.com>
2018-05-05 03:32:46 +0530

    start

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  url: any;
+  constructor(private sanitize: DomSanitizer) {}
+  urlpaste(){
+    this.url = "https://redart.co.uk/";
+    return this.sanitize.bypassSecurityTrustResourceUrl(this.url);
+  }
+}
